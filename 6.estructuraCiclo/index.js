05bb2491637ab8ae195325491721729ddfdb5a6d
rayda <raydahayde@gmail.com>
2022-10-25 11:07:47 -0500

    ejercicios de estructuradeciclos

---
+++ 6.estructuraCiclo/index.js
@@ -0,0 +1,16 @@
+//existen3
+// averiguar su consepto aplicacion y ejercicios
+// for
+for(let i=3;i<=10;i+=2){
+    console.log(i)
+}
+//while
+let eval =false
+let eval2='salir'
+while(eval==true){
+    if (eval2=='salir'){
+        console.log('adios')
+        eval=false
+    }
+}console.log('hola')
+//dowhile
