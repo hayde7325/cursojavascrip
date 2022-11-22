4a35d64025e2c4746acc71133fa1bc5380de1aa9
rayda <raydahayde@gmail.com>
2022-11-22 10:07:49 -0500

    ordenado

---
+++ 11.DOM/index.js
@@ -0,0 +1,24 @@
+function mostrar(){
+    document.getElementById("img").style.display = 'block'
+}
+function ocultar(){
+    document.getElementById("img").style.display = 'none'
+}
+function mostrar1(){
+    document.getElementById("img1").style.display = 'block'
+}
+function ocultar1(){
+    document.getElementById("img1").style.display = 'none'
+}
+function mostrar2(){
+    document.getElementById("img2").style.display = 'block'
+}
+function ocultar2(){
+    document.getElementById("img2").style.display = 'none'
+}
+function mostrar3(){
+    document.getElementById("img3").style.display = 'block'
+}
+function ocultar3(){
+    document.getElementById("img3").style.display = 'none'
+}
---
+++ 12.elementos/indes.js
@@ -0,0 +1,10 @@
+let newelement= documenttos .createElement("h1")
+document.body.appendChild(newelement).innertext="hola"
+let elementosPadre =document.querySelector("# contenido")
+let elementohijo=document.createElement("h1")
+let nuevoElemento=elementosPadre,appendChild(elementohijo)
+nuevoElemento.innertext=
+// otra manera
+elementos.appendChild(nuevoElemento)
+console.log(elemento)
+uniElemento.innertext
